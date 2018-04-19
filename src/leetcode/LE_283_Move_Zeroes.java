588e9cdac96b6528ee883724cdd66fc7bbed28e7
yuank <yuank@oath.com>
2018-04-19 00:58:31 -0700

    New checkin

---
+++ src/leetcode/LE_283_Move_Zeroes.java
@@ -0,0 +1,51 @@
+/**
+ * Created by yuank on 4/18/18.
+ */
+public class LE_283_Move_Zeroes {
+    /**
+     * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+     For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+     Note:
+     You must do this in-place without making a copy of the array.
+     Minimize the total number of operations
+
+     Easy
+     */
+
+    //Time : O(n), Space : O(1)
+
+    //Number of operations : nums.length
+    public void moveZeroes1(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int start = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[start++] = nums[i];//!!!
+            }
+        }
+
+        for (int i = start; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    //Number of operations : 2 * number of none-zero, better for case that has many zeros in nums
+    public void moveZeroes2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j++] = temp;
+            }
+        }
+    }
+}
---
+++ src/leetcode/LE_284_Peeking_Iterator.java
@@ -0,0 +1,108 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by yuank on 4/18/18.
+ */
+public class LE_284_Peeking_Iterator {
+    /**
+     * Given an Iterator class interface with methods: next() and hasNext(), design and implement a PeekingIterator
+     * that support the peek() operation -- it essentially peek() at the element that will be returned by the next call to next().
+
+         Here is an example. Assume that the iterator is initialized to the beginning of the list: [1, 2, 3].
+
+         Call next() gets you 1, the first element in the list.
+
+         Now you call peek() and it returns 2, the next element. Calling next() after that still return 2.
+
+         You call next() the final time and it returns 3, the last element. Calling hasNext() after that should return false.
+
+         ??Follow up: How would you extend your design to be generic and work with all types, not just integer?
+
+         Medium
+     */
+
+    class PeekingIterator implements Iterator<Integer> {
+
+        Iterator<Integer> iter;
+        Integer next = null;
+
+        public PeekingIterator(Iterator<Integer> iterator) {
+            // initialize any member here.
+            iter = iterator;
+            if (iter.hasNext()) {
+                next = iter.next();
+            }
+        }
+
+        // Returns the next element in the iteration without advancing the iterator.
+        public Integer peek() {
+            return next;
+        }
+
+        // hasNext() and next() should behave the same as in the Iterator interface.
+        // Override them if needed.
+        @Override
+        public Integer next() {
+            int res = next;
+            next = iter.hasNext() ? iter.next() : null;//!!!
+            return res;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return next != null; //!!! "!="
+        }
+    }
+
+
+    /**
+     * The first solution does not consider the case that NULL could be a valid value.
+     *
+     * using null to check if we have reached the end of iterator is not 100% right.
+     * In theory, null could be a valid element.An slightly alternative approach to use boolean indicating end of iterator:
+     *
+     */
+    class PeekingIterator1 implements Iterator<Integer> {
+        Integer next;
+        Iterator<Integer> iter;
+        boolean noSuchElement;
+
+        public PeekingIterator1(Iterator<Integer> iterator) {
+            // initialize any member here.
+            iter = iterator;
+            advanceIter();
+        }
+
+        // Returns the next element in the iteration without advancing the iterator.
+        public Integer peek() {
+            // you should confirm with interviewer what to return/throw
+            // if there are no more values
+            return next;
+        }
+
+        // hasNext() and next() should behave the same as in the Iterator interface.
+        // Override them if needed.
+        @Override
+        public Integer next() {
+            if (noSuchElement)
+                throw new NoSuchElementException();
+            Integer res = next;
+            advanceIter();
+            return res;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !noSuchElement;
+        }
+
+        private void advanceIter() {
+            if (iter.hasNext()) {
+                next = iter.next();
+            } else {
+                noSuchElement = true;
+            }
+        }
+    }
+}
