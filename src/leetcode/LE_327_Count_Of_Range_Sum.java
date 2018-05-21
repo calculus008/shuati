4d645b70c2784cfea5347b0064a326239106f5de
yuank <yuank@oath.com>
2018-05-21 00:32:20 -0700

    New checkin

--- src/leetcode/LE_327_Count_Of_Range_Sum.java
+++ src/leetcode/LE_327_Count_Of_Range_Sum.java
@@ -130,4 +130,69 @@ public class LE_327_Count_Of_Range_Sum {
         }
         return sum;
     }
+
+
+    /**
+     * Solution 2 : Merge Sort
+         https://leetcode.com/problems/count-of-range-sum/discuss/77990/Share-my-solution
+
+         The merge sort based solution counts the answer while doing the merge. During the merge stage, we have already sorted the left half [start, mid)
+         and right half [mid, end). We then iterate through the left half with index i. For each i, we need to find two indices k and j in the right half
+         where :
+
+             j is the first index satisfy sums[j] - sums[i] > upper and
+             k is the first index satisfy sums[k] - sums[i] >= lower.
+
+         Then the number of sums in [lower, upper] is j-k.
+
+         We also use another index t to copy the elements satisfy sums[t] < sums[i] to a cache in order
+         to complete the merge sort.
+
+         Despite the nested loops, the time complexity of the "merge & count" stage is still linear. Because the indices k, j, t will only increase
+         but not decrease, each of them will only traversal the right half once at most. The total time complexity of this divide and conquer
+         solution is then O(n log n).
+
+         One other concern is that the sums may overflow integer. So we use long instead.
+     *
+     */
+    public int countRangeSum2(int[] nums, int lower, int upper) {
+        int n = nums.length;
+        long[] sums = new long[n + 1];
+        for (int i = 0; i < n; ++i)
+            sums[i + 1] = sums[i] + nums[i];
+        return countWhileMergeSort(sums, 0, n + 1, lower, upper);
+    }
+
+    private int countWhileMergeSort(long[] sums, int start, int end, int lower, int upper) {
+        if (end - start <= 1) return 0;
+        int mid = (start + end) / 2;
+
+        int count = countWhileMergeSort(sums, start, mid, lower, upper)
+                + countWhileMergeSort(sums, mid, end, lower, upper);
+
+        int j = mid, k = mid, t = mid;
+        long[] cache = new long[end - start];
+
+        for (int i = start, r = 0; i < mid; ++i, ++r) {
+            // k is the first index satisfy sums[k] - sums[i] >= lower.
+            while (k < end && sums[k] - sums[i] < lower) {
+                k++;
+            }
+
+            //j is the first index satisfy sums[j] - sums[i] > upper and
+            while (j < end && sums[j] - sums[i] <= upper) {
+                j++;
+            }
+
+            while (t < end && sums[t] < sums[i]) {
+                cache[r++] = sums[t++];
+            }
+            cache[r] = sums[i];
+
+            count += j - k;
+        }
+        System.arraycopy(cache, 0, sums, start, t - start);
+        return count;
+    }
+
 }
