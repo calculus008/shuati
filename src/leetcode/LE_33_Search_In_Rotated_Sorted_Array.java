e16f17ec407e4cefe15e90c3f69911959dece10c
yuank <yuank@oath.com>
2018-06-25 23:35:39 -0700

    New checkin

--- src/leetcode/LE_33_Search_In_Rotated_Sorted_Array.java
+++ src/leetcode/LE_33_Search_In_Rotated_Sorted_Array.java
@@ -45,4 +45,44 @@ public class LE_33_Search_In_Rotated_Sorted_Array {
         if (nums[end] == target) return end;
         return -1;
     }
+
+    /**
+     * jiuzhang solution
+     */
+
+    public int search_JiuZhang(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+
+            if (nums[start] < nums[mid]) {
+                if (nums[start] <= target && target <= nums[mid]) {
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[end]) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            }
+        }
+
+        if (nums[start] == target) {
+            return start;
+        }
+
+        if (nums[end] == target) {
+            return end;
+        }
+
+        return -1;
+    }
 }
