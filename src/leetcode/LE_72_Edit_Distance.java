3938512a92bb421dac38043a6629bcab34f40f7f
yuank <yuank@oath.com>
2018-07-02 12:24:31 -0700

    New checkin

--- src/leetcode/LE_72_Edit_Distance.java
+++ src/leetcode/LE_72_Edit_Distance.java
@@ -4,7 +4,7 @@ package leetcode;
  * Created by yuank on 3/5/18.
  */
 public class LE_72_Edit_Distance {
-    /*
+    /**
         Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. (each operation is counted as 1 step.)
 
         You have the following 3 operations permitted on a word:
@@ -12,29 +12,43 @@ public class LE_72_Edit_Distance {
         a) Insert a character
         b) Delete a character
         c) Replace a character
-     */
+     **/
 
-//DP Solution : Time : O(m * n), Space : O(m * n)
+    /**
+        DP Solution : Time : O(m * n), Space : O(m * n)
 
-//"convert word1 to word2", so all actions (insert, delete and replace) are done on word1
-//dp[i][j] : min steps needed to convert word1 (1 - i) to word2 (1 - j) .
-//Insert :
-//    word1 a
-//    word2 ab
-//    need to insert b at index 1. In word1, cur pointer (i) points to 'a', in word2, cur pointer (j) points to 'b'.
-//    Therefore, to insert 'b', it is one more steps from "a", "a". dp[i][j - 1] + 1
-//delete :
-//    word1 ab
-//    word2 a
-//    need to delete b at index 1. In word1, cur pointer (i) points to 'b', in word2, cur pointer (j) points to 'a'.
-//    Therefore, to delete 'b', it is one more steps from "a", "a". dp[i - 1 ][j] + 1
-//replace :
-//    word1 ac
-//    word2 ab
-//    need to replace c with b at index 1. In word1, cur pointer (i) points to 'c', in word2, cur pointer (j) points to 'b'.
-//    Therefore, to replace 'c' with 'b', it is one more steps from "a", "a". dp[i - 1][j - 1] + 1
+        "convert word1 to word2", so all actions (insert, delete and replace) are done on word1
+        dp[i][j] : min steps needed to convert word1 (1 ~ i) to word2 (1 ~ j) .
 
-/*
+        http://zxi.mytechroad.com/blog/dynamic-programming/leetcode-72-edit-distance/
+
+       D("abbc", "acc")
+       = D("abb", "ac")
+       = 1 + min ( D("ab", "ac")   //Delete : delete "b" from word1
+                   D("abb", "a")   //Insert : if we know how to convert "abb" to "a",
+                                   //         then we just need to insert "c" at the end of word1
+                   D("ab",  "a"))  //Replace : if we know how to convert "ab" to "a",
+                                              then we just need to replace the last "b" in word1 with "c"
+
+        d(i, j) = minDistance(word1[0..i-1], word2[0..j-1])
+        1.if j = 0, i
+        2.if i = 0, j
+        3.if word1[i -1] = word2[j -1]  d(i - i, j - 1)
+        4.min (d(i - 1, j), d(i, j - 1), d(i - 1, j - 1) + 1
+
+        d(i, j) 表示把 str1 前 i 个字符编辑成 str2 前 j 个字符所需要的最小 edit distance
+
+        d(2, 1) = 1 + d(1, 1)
+        edit("ab", "a") = 1 + edit("a", "a") <- delete "b" from str1
+
+        d(1, 2) = 1 + d(1, 1)
+        edit("a", "ab") = 1 + edit("a", "a") <- insert "b" into str1
+
+        d(2, 2) = 1 + d(1, 1)
+        edit("ab", "ac") = 1 + edit("a", "a") <- replace "b" with "c" in str1﻿
+     **/
+
+/**
     "0" at dp[0]0] represents empty string, the init process is using delete action, for example : abcd, i=0, "a", one delete to get to "";
     i=1, "ab", two deletes to get to "";....
 
@@ -43,7 +57,7 @@ public class LE_72_Edit_Distance {
      a  1  0 1 2 3
      e  2  1 1 2 3
      f  3  2 2 2 3
- */
+ **/
 
     public static int minDistance(String word1, String word2) {
         int[][] dp = new int[word1.length() + 1][word2.length() + 1];
