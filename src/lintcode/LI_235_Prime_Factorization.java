c090c2e9dcf4e791f78b898005813096969ed04f
yuank <yuank@oath.com>
2018-07-03 01:06:24 -0700

    New checkin

---
+++ src/lintcode/LI_235_Prime_Factorization.java
@@ -0,0 +1,36 @@
+package lintcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yuank on 7/3/18.
+ */
+public class LI_235_Prime_Factorization {
+    /**
+         Prime factorize a given integer.
+         将一个整数分解为若干质因数之乘积
+
+         Example
+         Given 10, return [2, 5].
+
+         Given 660, return [2, 2, 3, 5, 11].
+     */
+
+    public List<Integer> primeFactorization(int num) {
+        List<Integer> factors = new ArrayList<Integer>();
+
+        for (int i = 2; i * i <= num; i++) {
+            while (num % i == 0) {
+                num = num / i;
+                factors.add(i);
+            }
+        }
+
+        if (num != 1) {
+            factors.add(num);
+        }
+
+        return factors;
+    }
+}
