acd818b3159a5a1844800233d0dc890d409dbc18
yuank <yuank@oath.com>
2018-07-06 00:28:01 -0700

    New checkin

--- src/leetcode/LE_69_Sqrt.java
+++ src/leetcode/LE_69_Sqrt.java
@@ -35,4 +35,24 @@ public class LE_69_Sqrt {
 
         return h;
     }
+
+    //使用九章binary search模版
+    public int sqrt_JiuZhang(int x) {
+        if (x <= 1) return x;
+
+        int start = 1;
+        int end = x;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (mid == x / mid) {
+                return mid;
+            } else if (mid > x / mid) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+
+        return start;
+    }
 }
