d28e51f5cc2b5248e8c21a37927a056ca9f9f782
yuank <yuank@oath.com>
2018-07-12 00:46:30 -0700

    New checkin

---
+++ src/common/ListNode.java
@@ -0,0 +1,10 @@
+package common;
+
+/**
+ * Created by yuank on 3/4/18.
+ */
+public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+}
