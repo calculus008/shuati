4fd8958ec1065a519d0a8b788f742a42cda5321e
yuank <yuank@oath.com>
2018-07-14 00:44:57 -0700

    New checkin

--- src/leetcode/LE_207_Course_Schedule.java
+++ src/leetcode/LE_207_Course_Schedule.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -7,7 +8,7 @@ import java.util.Queue;
  * Created by yuank on 3/26/18.
  */
 public class LE_207_Course_Schedule {
-    /*
+    /**
         There are a total of n courses you have to take, labeled from 0 to n - 1.
 
         Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
@@ -26,7 +27,7 @@ public class LE_207_Course_Schedule {
         Note:
         The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
         You may assume that there are no duplicate edges in the input prerequisites.
-     */
+     **/
 
     /**
     Topological sorting, https://www.geeksforgeeks.org/topological-sorting/
@@ -59,6 +60,61 @@ public class LE_207_Course_Schedule {
                     if (indegree[pair[0]] == 0) {
                         queue.offer(pair[0]);
                     }
+                }
+            }
+        }
+
+        return res == 0;
+    }
+
+    /**
+     * The 1st Solution TLE on lintcode for a test case with extreme large data set.
+     * The reason is : it does not have a data structure that saves neighbour info (the node I point to),
+     * instead, it uses for loop to go through entire given array.
+     *
+     * The following solution adds "edges" (array of ArrayList) for storing neighbours
+     */
+    public boolean canFinish_JiuZhang(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        int res = numCourses;
+
+        //Create datastructure to store neighour info
+        ArrayList[] edges = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            edges[i] = new ArrayList<Integer>();
+        }
+
+        //Get indgree for all nodes
+        for (int[] pair : prerequisites) {
+            indegree[pair[0]]++;
+            /**
+             * new:
+             * 我的邻居是我指向的那个
+             */
+            edges[pair[1]].add(pair[0]);
+        }
+
+        //Find starting nodes which have 0 indgree
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            res--;
+
+            /**
+             * new
+             * Check neighbours directly
+             */
+            ArrayList<Integer> neighour = edges[cur];
+            for (int e : neighour) {
+                indegree[e]--;
+                if (indegree[e] == 0) {
+                    queue.offer(e);
                 }
             }
         }
