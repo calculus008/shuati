2c773db6ad57e2910b8e8c8caa50b221250390f2
yuank <yuank@oath.com>
2018-07-16 13:05:03 -0700

    New checkin

---
+++ src/common/Coordinate.java
@@ -0,0 +1,13 @@
+package common;
+
+/**
+ * Created by yuank on 7/16/18.
+ */
+public class Coordinate {
+    public int x, y;
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/common/DirectedGraphNode.java
@@ -0,0 +1,10 @@
+package common;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yuank on 7/13/18.
+ */
+public class DirectedGraphNode {
+    public ArrayList<DirectedGraphNode> neighbors;
+}
