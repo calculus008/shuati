dc46fd17df4d91ce6c4185cafc7e6574ba2275f5
yuank <yuank@oath.com>
2018-07-18 01:31:30 -0700

    New checkin

--- src/common/DirectedGraphNode.java
+++ src/common/DirectedGraphNode.java
@@ -6,5 +6,7 @@ import java.util.ArrayList;
  * Created by yuank on 7/13/18.
  */
 public class DirectedGraphNode {
+    public int label;
     public ArrayList<DirectedGraphNode> neighbors;
+    public DirectedGraphNode(int x) { label = x; neighbors = new ArrayList<DirectedGraphNode>(); }
 }
---
+++ src/common/Point.java
@@ -0,0 +1,11 @@
+package common;
+
+/**
+ * Created by yuank on 7/18/18.
+ */
+public class Point {
+    public int x;
+    public int y;
+    public Point() { x = 0; y = 0; }
+    public Point(int a, int b) { x = a; y = b; }
+}
