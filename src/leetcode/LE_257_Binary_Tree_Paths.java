1baa97bce134ecf03664439138affcb9404a1255
yuank <yuank@oath.com>
2018-07-23 12:11:42 -0700

    add StringBuilder version

--- src/leetcode/LE_257_Binary_Tree_Paths.java
+++ src/leetcode/LE_257_Binary_Tree_Paths.java
@@ -47,4 +47,56 @@ public class LE_257_Binary_Tree_Paths {
             helper(root.right, res, path + root.val + "->");
         }
     }
+
+    /**
+     * StringBuilder version, the trick is :
+     * 1.Remember length of StringBuilder before recursion starts.
+     * 2.Use "setLength" to recover to original state after EACH recursion of the next level returns.
+     */
+    public List<String> binaryTreePaths_2(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        if (root == null) return res;
+
+        helper(root, res, new StringBuilder());
+        return res;
+    }
+
+    public void helper(TreeNode root, List<String> res, StringBuilder sb) {
+        /**
+         * 1.Base case : check if right and left children are both null
+         */
+        if (root.right == null && root.left == null) {
+            res.add(sb.append(root.val).toString());
+            return;
+        }
+
+        /**
+         * 3.Record stringbuilder length before next level recursion
+         */
+        int len = sb.length();
+
+        /**
+         * 2.Add check null for left and right children before recursion :
+         *   a.avoid null pointer exception in the next level recursion;
+         *   b."sb.append(root.val).append("->")", if child is empty, this line won't execute,
+         *      so we don't need to worry about path ending like "1->2->3->"
+         */
+        if (root.left != null) {
+            helper(root.left, res, sb.append(root.val).append("->"));
+        }
+
+        /**
+         * 3.Recover stringbuilder value after each recursion
+         */
+        sb.setLength(len);
+
+        if (root.right != null) {
+            helper(root.right, res, sb.append(root.val).append("->"));
+        }
+
+        /**
+         * 3.Recover stringbuilder value after each recursion
+         */
+        sb.setLength(len);
+    }
 }
