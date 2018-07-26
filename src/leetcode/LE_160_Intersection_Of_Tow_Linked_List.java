3fd85afba4c458336c206334865d7acc3184cb07
yuank <yuank@oath.com>
2018-07-26 00:08:38 -0700

    New checkin

--- src/leetcode/LE_160_Intersection_Of_Tow_Linked_List.java
+++ src/leetcode/LE_160_Intersection_Of_Tow_Linked_List.java
@@ -4,7 +4,7 @@ package leetcode;
  * Created by yuank on 3/19/18.
  */
 public class LE_160_Intersection_Of_Tow_Linked_List {
-    /*
+    /**
         Write a program to find the node at which the intersection of two singly linked lists begins.
 
 
@@ -28,6 +28,11 @@ public class LE_160_Intersection_Of_Tow_Linked_List {
 
     //Time : O(m + n), Space : O(1)
 
+    /**
+     * Solution 1
+     * Get length of both lists, move on the longer one until it is the same length as the shorter one,
+     * then move both until the nodes from the 2 lists equal for the first time.
+     */
     public ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
         int lenA = 1, lenB = 1;
         ListNode la = headA;
@@ -58,6 +63,45 @@ public class LE_160_Intersection_Of_Tow_Linked_List {
         while (la != lb) {
             la = la.next;
             lb = lb.next;
+        }
+
+        return la;
+    }
+
+    /**
+     * Solution 2
+     *
+     * 1.Intersection exists
+     *   For each list, when come to the end, go to the head of the other list (create a circle of length m + n if intersection exists)
+     *
+     *     ____________________
+     *    |                    |
+     * A  1 -> 2 -> 3          |
+     *               \         |
+     *               7 -> 8 -> 9
+     *              /          |
+     * B      5 -> 6           |
+     *        |________________|
+     *
+     * 2.No intersection
+     *  "la == lb" happens when it reaches the end of the list, la is null
+     *
+     * It works because pointer A walks through List A and List B (since once it hits null, it goes to List B's head).
+     * Pointer B also walks through List B and List A.
+     * Regardless of the length of the two lists, the sum of the lengths are the same (i.e. a+b = b+a),
+     * which means that the pointers sync up at the point of intersection.
+     * If the lists never intersected, it's fine too, because they'll sync up at the end of each list, both of which are null.
+     *
+     */
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode la = headA;
+        ListNode lb = headB;
+
+        while (la != lb) {
+            la = la == null ? headB : la.next;
+            lb = lb == null ? headA : lb.next;
         }
 
         return la;
