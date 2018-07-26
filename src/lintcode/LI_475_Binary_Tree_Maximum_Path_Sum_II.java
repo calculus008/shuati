fee82047e0094a7642f6a1ca8cacef7aa178f249
yuank <yuank@oath.com>
2018-07-26 12:51:23 -0700

    New checkin

---
+++ src/lintcode/LI_475_Binary_Tree_Maximum_Path_Sum_II.java
@@ -0,0 +1,36 @@
+package lintcode;
+
+import leetcode.TreeNode;
+
+/**
+ * Created by yuank on 7/26/18.
+ */
+public class LI_475_Binary_Tree_Maximum_Path_Sum_II {
+    /**
+         Given a binary tree, find the maximum path sum from root.
+
+         The path may end at any node in the tree and contain at least one node in it.
+
+         Example
+         Given the below binary tree:
+
+           1
+          / \
+         2   3
+         return 4. (1->3)
+
+         Easy
+     */
+
+    /** Compare with :*
+     *  LE_124_Binary_Tree_Max_Path_Sum
+     **/
+    public int maxPathSum2(TreeNode root) {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+        int left = Math.max(0, maxPathSum2(root.left));
+        int right = Math.max(0, maxPathSum2(root.right));
+        return Math.max(left, right) + root.val;
+    }
+}
