9eb4ad36bbc4712f099668789ace4a572955019e
yuank <yuank@oath.com>
2018-07-30 11:48:13 -0700

    New checkin

--- src/leetcode/LE_78_Subsets.java
+++ src/leetcode/LE_78_Subsets.java
@@ -1,13 +1,12 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by yuank on 3/5/18.
  */
 public class LE_78_Subsets {
-    /*
+    /**
         Given a set of DISTINCT integers, nums, return all possible subsets (the power set).
 
         Note: The solution set must not contain duplicate subsets.
@@ -27,23 +26,30 @@ public class LE_78_Subsets {
         ]
      */
 
-    //Time : O(2 ^ n)
-    //Example : [1, 2, 3]
-    //[]
-    //[1]
-    //[1,2]
-    //[1,2,3]
-    //[1,3]
-    //[2]
-    //[2,3]
-    //[3]
+    /**
+     * Soltuion 1 : DFS
+     * Time : O(2 ^ n)
+       Example : [1, 2, 3]
+         []
 
-    //1-2-3
-    //| |
-    //2 3
-    //|
-    //3
+         [1]
+         [1,2]
+         [1,2,3]
 
+         [1,3]
+
+         [2]
+         [2,3]
+
+         [3]
+
+         把给定元素看成图的结构：
+         1-2-3
+         | |
+         2 3
+         |
+         3
+     */
     public static List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         if(nums == null || nums.length == 0) return res;
@@ -59,5 +65,78 @@ public class LE_78_Subsets {
             helper(res, nums, temp, i + 1);
             temp.remove(temp.size() - 1);
         }
+    }
+
+    /**
+     * Solution 2 : BFS
+     * 用 BFS 来解决该问题时，层级关系如下：
+         第一层: []
+         第二层: [1] [2] [3]
+         第三层: [1, 2] [1, 3], [2, 3]
+         第四层: [1, 2, 3]
+
+
+         每一层的节点都是上一层的节点拓展而来。
+     */
+    public List<List<Integer>> subsets2_JiuZhang(int[] nums) {
+        // List vs ArrayList （google）
+        List<List<Integer>> results = new LinkedList<>();
+
+        if (nums == null) {
+            return results; // 空列表
+        }
+
+        //!!! "subset.get(subset.size() - 1) < nums[i]" is based on the fact that nums is sorted.
+        Arrays.sort(nums);
+
+        // BFS
+        Queue<List<Integer>> queue = new LinkedList<>();
+        queue.offer(new LinkedList<Integer>());
+
+        while (!queue.isEmpty()) {
+            List<Integer> subset = queue.poll();
+            results.add(subset);
+
+            for (int i = 0; i < nums.length; i++) {
+                if (subset.size() == 0 || subset.get(subset.size() - 1) < nums[i]) {
+                    List<Integer> nextSubset = new LinkedList<Integer>(subset);
+                    nextSubset.add(nums[i]);
+                    queue.offer(nextSubset);
+                }
+            }
+        }
+
+        return results;
+    }
+
+    /**
+     * Solution 3 : 非递归版本，利用二进制的方式逐个枚举 subsets。
+     * @param nums
+     * @return
+     */
+
+    public List<List<Integer>> subsets3_JiuZhang(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        // 1 << n is 2^n
+        // each subset equals to an binary integer between 0 .. 2^n - 1
+        // 0 -> 000 -> []
+        // 1 -> 001 -> [1]
+        // 2 -> 010 -> [2]
+        // ..
+        // 7 -> 111 -> [1,2,3]
+        for (int i = 0; i < (1 << n); i++) {
+            List<Integer> subset = new ArrayList<Integer>();
+            for (int j = 0; j < n; j++) {
+                // check whether the jth digit in i's binary representation is 1
+                if ((i & (1 << j)) != 0) {
+                    subset.add(nums[j]);
+                }
+            }
+            result.add(subset);
+        }
+        return result;
     }
 }
