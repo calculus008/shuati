10f4e2a5e07d7b9f3ed8e272abd9e710a446ffbe
yuank <yuank@oath.com>
2018-08-04 11:48:46 -0700

    New checkin

--- src/leetcode/LE_44_Wildcards_Matching.java
+++ src/leetcode/LE_44_Wildcards_Matching.java
@@ -4,7 +4,7 @@ package leetcode;
  * Created by yuank on 2/27/18.
  */
 public class LE_44_Wildcards_Matching {
-    /*
+    /**
         Implement wildcard pattern matching with support for '?' and '*'.
 
         '?' Matches any single character.
@@ -115,4 +115,66 @@ public class LE_44_Wildcards_Matching {
     }
 
 
+    /**
+     * Solution 2
+     * 使用深度优先搜索 + 记忆化的版本。
+     * 用一个二维的 boolean 数组来当记忆化数组，记录 s 从 sIndex 开始的后缀 能够匹配上 p 从 pIndex 开始的后缀
+     */
+    public boolean isMatch2_JiuZhang(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+
+        boolean[][] memo = new boolean[s.length()][p.length()];
+        boolean[][] visited = new boolean[s.length()][p.length()];
+        return isMatchHelper(s, 0, p, 0, memo, visited);
+    }
+
+    private boolean isMatchHelper(String s, int sIndex,
+                                  String p, int pIndex,
+                                  boolean[][] memo,
+                                  boolean[][] visited) {
+        // 如果 p 从pIdex开始是空字符串了，那么 s 也必须从 sIndex 是空才能匹配上
+        if (pIndex == p.length()) {
+            return sIndex == s.length();
+        }
+
+        // 如果 s 从 sIndex 是空，那么p 必须全是 *
+        if (sIndex == s.length()) {
+            return allStar(p, pIndex);
+        }
+
+        if (visited[sIndex][pIndex]) {
+            return memo[sIndex][pIndex];
+        }
+
+        char sChar = s.charAt(sIndex);
+        char pChar = p.charAt(pIndex);
+        boolean match;
+
+        if (pChar == '*') {
+            match = isMatchHelper(s, sIndex, p, pIndex + 1, memo, visited) ||
+                    isMatchHelper(s, sIndex + 1, p, pIndex, memo, visited);
+        } else {
+            match = charMatch(sChar, pChar) &&
+                    isMatchHelper(s, sIndex + 1, p, pIndex + 1, memo, visited);
+        }
+
+        visited[sIndex][pIndex] = true;
+        memo[sIndex][pIndex] = match;
+        return match;
+    }
+
+    private boolean charMatch(char sChar, char pChar) {
+        return (sChar == pChar || pChar == '?');
+    }
+
+    private boolean allStar(String p, int pIndex) {
+        for (int i = pIndex; i < p.length(); i++) {
+            if (p.charAt(i) != '*') {
+                return false;
+            }
+        }
+        return true;
+    }
 }
