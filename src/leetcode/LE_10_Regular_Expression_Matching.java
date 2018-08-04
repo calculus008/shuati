83ff0292eebf20569d315e652bad99451385510d
yuank <yuank@oath.com>
2018-08-04 11:56:14 -0700

    New checkin

---
+++ src/leetcode/LE_10_Regular_Expression_Matching.java
@@ -0,0 +1,123 @@
+package leetcode;
+
+/**
+ * Created by yuank on 8/4/18.
+ */
+public class LE_10_Regular_Expression_Matching {
+    /**
+         Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+
+         '.' Matches any single character.
+         '*' Matches zero or more of the preceding element.
+         The matching should cover the entire input string (not partial).
+
+         Note:
+
+         s could be empty and contains only lowercase letters a-z.
+         p could be empty and contains only lowercase letters a-z, and characters like . or *.
+         Example 1:
+
+         Input:
+         s = "aa"
+         p = "a"
+         Output: false
+         Explanation: "a" does not match the entire string "aa".
+
+         Example 2:
+
+         Input:
+         s = "aa"
+         p = "a*"
+         Output: true
+         Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+
+         Example 3:
+
+         Input:
+         s = "ab"
+         p = ".*"
+         Output: true
+         Explanation: ".*" means "zero or more (*) of any character (.)".
+
+         Example 4:
+
+         Input:
+         s = "aab"
+         p = "c*a*b"
+         Output: true
+         Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore it matches "aab".
+
+         Example 5:
+
+         Input:
+         s = "mississippi"
+         p = "mis*is*p*."
+         Output: false
+
+         Hard
+     */
+
+    class Solution {
+        /**
+            https://leetcode.com/problems/regular-expression-matching/discuss/5651
+            https://www.youtube.com/watch?v=c5vsle60Uw8&list=PLvyIyKZVcfAmSRMbPFWjWP6gBwPWiWwSU&index=10
+
+             1, If p.charAt(j) == s.charAt(i) :  dp[i][j] = dp[i-1][j-1];
+             2, If p.charAt(j) == '.' : dp[i][j] = dp[i-1][j-1];
+             3, If p.charAt(j) == '*':
+                here are two sub conditions:
+                        1   if p.charAt(j-1) != s.charAt(i) : dp[i][j] = dp[i][j-2]  //in this case, a* only counts as empty
+                        2   if p.charAt(i-1) == s.charAt(i) or p.charAt(i-1) == '.':
+                                       dp[i][j] = dp[i-1][j]    //in this case, a* counts as multiple a !!!
+                                    or dp[i][j] = dp[i][j-1]   // in this case, a* counts as single a
+                                    or dp[i][j] = dp[i][j-2]   // in this case, a* counts as empty
+
+           Example of case 3.2 ,a* counts as multiple a:
+           s : aaa, p : a*
+           i = 0, j = 0 : a equals a
+           i = 1, j = 1 : char at current j is '*', so we need to see if previous char in p matches current char in s ('*' is used as
+                 the previous char)
+           i = 2, j = 1 : check the matching state of previous char in s,'aaa' - 'a*', combine a in index 1 and 2 in s to match '*' in p
+         **/
+
+        public boolean isMatch(String s, String p) {
+            if (s == null || p == null) return false;
+
+            boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+            dp[0][0] = true;//!!!
+
+            /**
+                pre-process for case 3.1 when i is 0, init dp[0][j] (since i starts from 1 in the main for loop)
+                dp[0][j] : empty string matches with pattern string.
+                dp[0][j - 1] == true, then current '*' can take the meaning of matching 0 previous char('*' as empty), so dp[0][j] is true
+                dp[0][j-2] == true, then 'a*' as empty, so dp[0][j] = true
+             **/
+            for (int j = 1; j < dp[0].length; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    if (dp[0][j - 1] || (j > 1 && dp[0][j - 2])) {
+                        dp[0][j] = true;
+                    }
+                }
+            }
+
+            for (int i = 1; i < dp.length; i++) {
+                for (int j = 1; j < dp[0].length; j++) {
+                    if(s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+
+                    if(p.charAt(j - 1) == '*') {
+                        if(s.charAt(i - 1) != p.charAt(j - 2) && p.charAt(j - 2) != '.') {
+                            dp[i][j] = dp[i][j - 2]; //Case 3.1 : a* only counts as empty, so takes status of j-2 (j moves back 2)
+                        } else {
+                            dp[i][j] = (dp[i][j - 1] || dp[i][j - 2] || dp[i - 1][j]);
+                        }
+                    }
+
+                }
+            }
+
+            return dp[s.length()][p.length()];
+        }
+    }
+}
