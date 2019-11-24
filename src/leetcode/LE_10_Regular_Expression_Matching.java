3ff3615750de0cc8d77e9f687a89e4df26c597fa
yuank <yuank@yahoo.com>
2019-11-24 14:57:22 -0800

    new

--- src/leetcode/LE_10_Regular_Expression_Matching.java
+++ src/leetcode/LE_10_Regular_Expression_Matching.java
@@ -57,6 +57,46 @@ public class LE_10_Regular_Expression_Matching {
          Hard
      */
 
+    class Solution_Practice {
+        public boolean isMatch(String s, String p) {
+            if (s == null || p == null) return false;
+
+            int m = s.length();
+            int n = p.length();
+            boolean[][] dp = new boolean[m + 1][n + 1];
+            dp[0][0] = true;
+
+            for (int j = 2; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    dp[0][j] = dp[0][j - 2];
+                }
+            }
+
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    char sc = s.charAt(i - 1);
+                    char pc = p.charAt(j - 1);
+
+                    if (sc == pc || pc == '.') {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else if (pc == '*') {
+                        if (dp[i][j - 2]) {
+                            /**
+                             * if making 'a*' as empty string in p can match with s,
+                             * dp[i][j] is true.
+                             */
+                            dp[i][j] = true;
+                        } else if (sc == p.charAt(j - 2) || p.charAt(j - 2) == '.') {
+                            dp[i][j] = dp[i - 1][j];
+                        }
+                    }
+                }
+            }
+
+            return dp[m][n];
+        }
+    }
+
     class Solution {
         /**
          *  Solution 1
@@ -196,5 +236,4 @@ public class LE_10_Regular_Expression_Matching {
         }
         return true;
     }
-
 }
