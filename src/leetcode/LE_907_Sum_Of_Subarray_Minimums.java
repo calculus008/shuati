9263b51ad142247893e98e944279c8ee0132dfec
Kang Yuan <kyuan4@apple.com>
2021-05-20 11:09:17 -0700

    LE

--- src/leetcode/LE_907_Sum_Of_Subarray_Minimums.java
+++ src/leetcode/LE_907_Sum_Of_Subarray_Minimums.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class LE_907_Sum_Of_Subarray_Minimums {
@@ -13,7 +16,6 @@ public class LE_907_Sum_Of_Subarray_Minimums {
      * Output: 17
      * Explanation: Subarrays are [3], [1], [2], [4], [3,1], [1,2], [2,4], [3,1,2], [1,2,4], [3,1,2,4].
      * Minimums are 3, 1, 2, 4, 1, 1, 2, 1, 1, 1.  Sum is 17.
-     *
      *
      * Note:
      * 1 <= A.length <= 30000
@@ -30,7 +32,6 @@ public class LE_907_Sum_Of_Subarray_Minimums {
      * Example 2:
      * Input: arr = [11,81,94,43,3]
      * Output: 444
-     *
      *
      * Constraints:
      * 1 <= arr.length <= 3 * 104
@@ -51,9 +52,82 @@ public class LE_907_Sum_Of_Subarray_Minimums {
      * LE_769_Max_Chunks_To_Make_Sorted
      */
 
+    class Demo {
+        /**
+         * Mono Increase Stack is used for:
+         * Find the Previous Less Element (PLE) of each element in a list (or array) with O(n) time.
+         * For example:
+         * [3, 7, 8, 4]
+         * The previous less element of 7 is 3.
+         * The previous less element of 8 is 7.
+         * The previous less element of 4 is 3.
+         *
+         * -1 in res : no PLE
+         *
+         * Push index, not element itself, into stack.
+         * "num" represents the number pushed into the stack at current stack (after add PLE to res):
+         * stack           res
+         * "0"             -1
+         *  0 "1"          A[0] = 3
+         *  0  1 "2"       A[1] = 7
+         *  0 "3"          A[0] = 3
+         */
+        public List<Integer> findPreviousLessElements(int[] A) {
+            List<Integer> res = new ArrayList<>();
+            Stack<Integer> stack = new Stack<>();
+            for (int i = 0; i < A.length; i++) {
+                while (!stack.isEmpty() && A[stack.peek()] > A[i]) {
+                    stack.pop();
+                }
+                int PLE = stack.isEmpty() ? -1 : A[stack.peek()];
+                res.add(PLE);
+
+                stack.push(i);
+            }
+            return res;
+        }
+
+        /**
+         * Find the Next Less Element (NLE) of each element in a list (or array) with O(n) time:
+         * What is the next less element of an element?
+         * For example:
+         * [3, 7, 8, 4]
+         * The next less element of 8 is 4.
+         * The next less element of 7 is 4.
+         * There is no next less element for 3 and 4.
+         *
+         * stack            res
+         * "0"              -
+         *  0 "1"           -
+         *  0  1  "2"       -
+         *  0 "3"           res[2] = A[3] = 4
+         *                  res[1] = A[3] = 4
+         *
+         *                  [-1, 4, 4, -1]
+         */
+        public int[] findNextLessElements(int[] A) {
+            int[] res = new int[A.length];
+            Arrays.fill(res, -1);
+
+            Stack<Integer> stack = new Stack<>();
+            for (int i = 0; i < A.length; i++) {
+                while (!stack.isEmpty() && A[stack.peek()] > A[i]) {
+                    int top = stack.pop();
+                    res[top] = A[i];
+                }
+                stack.push(i);
+            }
+            return res;
+        }
+    }
+
     /**
      * https://leetcode.com/problems/sum-of-subarray-minimums/discuss/178876/stack-solution-with-very-detailed-explanation-step-by-step
      * Good summary of mono stack problems.
+     *
+     *
+     * 1.
+     * 2.find the next less element of each element in a vector with O(n) time
      *
      * Key:
      * For each arr[i], find previous less elements and next less elements, its contribution to the sum is pre * next * arr[i].
@@ -102,7 +176,6 @@ public class LE_907_Sum_Of_Subarray_Minimums {
      * How much the element 3 contributes to the final answer?
      * It is 3*(4*3).
      */
-
     class Solution {
         public int sumSubarrayMins(int[] arr) {
             int len = arr.length;
@@ -112,23 +185,41 @@ public class LE_907_Sum_Of_Subarray_Minimums {
             Stack<Integer> s1 = new Stack<>();
             Stack<Integer> s2 = new Stack<>();
 
-            int[] A = new int[len];
-            int[] B = new int[len];
+            /**
+             * distance from current index to the index of PLE
+             * If there's a PLE for current element, the distance is the length of subarray from the element right next
+             * to PLE element to current element. For example:
+             * [2, 9, 7, 8, 3, 4, 6, 1]
+             *     |________|
+             *
+             * For "3" (at index 4), its PLE is "2" (at index 0), the distance is 4 - 0 = 4
+             *
+             * If there's no PLE for current element, the distance is the length or total number of element from the
+             * frist element to current element.
+             */
+            int[] A = new int[len];//
+            int[] B = new int[len];//distance from current index to the index of NLE
 
             for (int i = 0; i < len; i++) {
-                A[i] = i + 1;
+                /**
+                 * From demo, we can see that PLE element is set for every index in for loop. So we don't really need
+                 * to init A[] here. For a given index, if stack is empty, it means there's no PLE for the elemnet at
+                 * the current index. For this problem, the distance to PLE is length of current index, which is i + 1.
+                 */
+                //A[i] = i + 1;
+
                 /**
                  * !!!
-                 * Must init B[], otherwise, for example : [1, 2, 3, 4, 5], it keeps increasing, so we never go into
-                 * while loop for s2, then we lose the chance to set B[], then it will has value of 0. Therefore, we
-                 * need to init with len - (i + 1) + 1, assuming current element is the largest from current position
-                 * to the end of the arr[].
+                 * From demo, we can see that we only set NLE when there's stack pop operations at certain index. If
+                 * there's no such action, it means there's no NLE, or current element is the largest from this index
+                 * to the end. For this problem it means the distance is:
+                 * total length of the array - (i + 1) + 1 => len - i
                  */
                 B[i] = len - i;
             }
 
             for (int i = 0; i < len; i++) {
-                //previous less
+                //previous less element
                 while (!s1.isEmpty() && arr[s1.peek()] > arr[i]) {
                     s1.pop();
                 }
@@ -139,7 +230,7 @@ public class LE_907_Sum_Of_Subarray_Minimums {
                  */
                 s1.push(i);
 
-                //next less
+                //next less element
                 while (!s2.isEmpty() && arr[s2.peek()] > arr[i]) {
                     B[s2.peek()] = i - s2.peek();
                     s2.pop();
