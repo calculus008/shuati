983e89f6643cf7eb750cb6bed3b74287cc133f36
Kang Yuan <kyuan4@apple.com>
2021-05-27 15:21:27 -0700

    LE

--- src/leetcode/LE_379_Design_Phone_Directory.java
+++ src/leetcode/LE_379_Design_Phone_Directory.java
@@ -48,7 +48,7 @@ public class LE_379_Design_Phone_Directory {
      * The get() method will look up first in the recycling set rather than assign a brand new number in bank. No need
      * to "initialize" any collection or array, so O(1) is garanteed.
      */
-    class PhoneDirectory {
+    class PhoneDirectory1 {
         int max;
         int next;
         Set<Integer> set;
@@ -56,7 +56,7 @@ public class LE_379_Design_Phone_Directory {
         /** Initialize your data structure here
          @param maxNumbers - The maximum numbers that can be stored in the phone directory.
          */
-        public PhoneDirectory(int maxNumbers) {
+        public PhoneDirectory1(int maxNumbers) {
             max = maxNumbers;
             next = 0;
             set = new HashSet<>();
@@ -105,6 +105,63 @@ public class LE_379_Design_Phone_Directory {
         private boolean isTaken(int number) {
             return number < next && !set.contains(number);
         }
+    }
 
+    /**
+     * https://leetcode.com/problems/design-phone-directory/discuss/122908/Java-O(1)-time-o(n)-space-single-Array-99ms-beats-100
+     * https://leetcode.com/problems/design-phone-directory/discuss/566643/Java-Free-List-approach.-O(1)-timebeats-100.
+     *
+     * Array
+     * It uses the idea of a Free List : https://en.wikipedia.org/wiki/Free_list
+     * We have allocated pool of numbers of given size. Each free number in array points to next free number (emulating linked list inside array).
+     *
+     * Initialization takes O(n), if you are asked to implement using an array, this should be the solution.
+     */
+    class PhoneDirectory2 {
+        int[] next;
+        int pos;
+        int max;
+
+        public PhoneDirectory2(int maxNumbers) {
+            max = maxNumbers;
+            next = new int[max];
+            for (int i = 0; i < max; i++) {
+                /**
+                 * "0 <= number < maxNumbers"
+                 */
+                next[i] = (i + 1) % max;
+            }
+            pos = 0;
+        }
+
+        public int get() {
+            if (next[pos] == -1) return -1;
+
+            //"pos" points to the current available unit, return it.
+            int ret = pos;
+            //move the "pos" to the next available unit
+            pos = next[pos];
+            //current unit is taken, mark it as -1
+            next[ret] = -1;
+            return ret;
+        }
+
+        public boolean check(int number) {
+            return isValid(number) && next[number] != -1;
+        }
+
+        public void release(int number) {
+            if (!isValid(number) || next[number] != -1) return;
+
+            //think it as putting the release unit to the front of the available linked list.
+            //pointing the release unit to the head of the available units list
+            next[number] = pos;
+            //set available units list head to the released unit
+            pos = number;
+        }
+
+        private boolean isValid(int number) {
+            return number >= 0 && number < max;
+        }
     }
 }
